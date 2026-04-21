a46c8e77e6c9b95a118b9a14e0fb0607926470dc
dKamilla <kamilla.dobos@stud.uni-corvinus.hu>
2026-04-22 00:00:39 +0200

    Első pár próba elem elhelyezése.

---
+++ kliensapp/FairOrder/FairOrder/Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace FairOrder
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            imageList1 = new ImageList(components);
+            textBox1 = new TextBox();
+            button1 = new Button();
+            listBox1 = new ListBox();
+            button2 = new Button();
+            SuspendLayout();
+            // 
+            // imageList1
+            // 
+            imageList1.ColorDepth = ColorDepth.Depth32Bit;
+            imageList1.ImageSize = new Size(16, 16);
+            imageList1.TransparentColor = Color.Transparent;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(28, 63);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(389, 27);
+            textBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(842, 61);
+            button1.Name = "button1";
+            button1.Size = new Size(94, 29);
+            button1.TabIndex = 1;
+            button1.Text = "button1";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // listBox1
+            // 
+            listBox1.FormattingEnabled = true;
+            listBox1.Location = new Point(662, 117);
+            listBox1.Name = "listBox1";
+            listBox1.Size = new Size(274, 264);
+            listBox1.TabIndex = 2;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(842, 412);
+            button2.Name = "button2";
+            button2.Size = new Size(94, 29);
+            button2.TabIndex = 3;
+            button2.Text = "button2";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(981, 453);
+            Controls.Add(button2);
+            Controls.Add(listBox1);
+            Controls.Add(button1);
+            Controls.Add(textBox1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private ImageList imageList1;
+        private TextBox textBox1;
+        private Button button1;
+        private ListBox listBox1;
+        private Button button2;
+    }
+}
