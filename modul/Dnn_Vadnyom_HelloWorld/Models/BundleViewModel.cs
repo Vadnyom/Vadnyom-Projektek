87979ebce5dc38f966c6aa87977ed6cb17aa1ca3
Berenike000 <berenike.bagi@stud.uni-corvinus.hu>
2026-04-22 16:08:44 +0200

    Add DNN HelloWorld module project

---
+++ modul/Dnn_Vadnyom_HelloWorld/Models/BundleViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+
+namespace Vadnyom.Dnn.Dnn_Vadnyom_HelloWorld.Models
+{
+    public class BundleViewModel
+    {
+        public int CurrentStep { get; set; }
+
+        public IEnumerable<Item> Coats { get; set; }
+        public IEnumerable<Item> Pants { get; set; }
+        public IEnumerable<Item> Boots { get; set; }
+
+        public int? SelectedCoatId { get; set; }
+        public int? SelectedPantsId { get; set; }
+        public int? SelectedBootId { get; set; }
+
+        public decimal OriginalTotal { get; set; }
+        public decimal DiscountAmount { get; set; }
+        public decimal DiscountedTotal { get; set; }
+
+        public bool HasCompleteBundle
+        {
+            get
+            {
+                return SelectedCoatId.HasValue
+                    && SelectedPantsId.HasValue
+                    && SelectedBootId.HasValue;
+            }
+        }
+    }
+}
---
+++ modul/Dnn_Vadnyom_HelloWorld/Models/Item.cs
@@ -0,0 +1,58 @@
+﻿/*
+' Copyright (c) 2026 Vadnyom
+'  All rights reserved.
+' 
+' THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED
+' TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+' THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF
+' CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+' DEALINGS IN THE SOFTWARE.
+' 
+*/
+
+using System;
+using System.Web.Caching;
+using DotNetNuke.Common.Utilities;
+using DotNetNuke.ComponentModel.DataAnnotations;
+using DotNetNuke.Entities.Content;
+
+namespace Vadnyom.Dnn.Dnn_Vadnyom_HelloWorld.Models
+{
+    [TableName("Dnn_Vadnyom_HelloWorld_Items")]
+    [PrimaryKey("ItemId", AutoIncrement = true)]
+    [Cacheable("Items", CacheItemPriority.Default, 20)]
+    [Scope("ModuleId")]
+    public class Item
+    {
+        public int ItemId { get; set; } = -1;
+       
+        public string ItemName { get; set; }
+
+        public string ItemDescription { get; set; }
+
+        public int AssignedUserId { get; set; }
+
+        public int ModuleId { get; set; }
+
+        public int CreatedByUserId { get; set; } = -1;
+
+        public int LastModifiedByUserId { get; set; } = -1;
+
+        public DateTime CreatedOnDate { get; set; } = DateTime.UtcNow;
+
+        public DateTime LastModifiedOnDate { get; set; } = DateTime.UtcNow;
+
+
+        // ÚJ MEZŐK A BUNDLE / TERMÉK DIZÁJNHOZ
+
+        public string Category { get; set; }
+
+        public decimal Price { get; set; }
+
+        public string ImageUrl { get; set; }
+
+        public int SortOrder { get; set; }
+
+        public bool IsActive { get; set; } = true;
+    }
+}
