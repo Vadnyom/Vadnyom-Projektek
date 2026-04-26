1f2f321f7f993cd5a30a778066f2ee2cd1e7b15e
Berenike000 <berenike.bagi@stud.uni-corvinus.hu>
2026-04-27 01:04:39 +0200

    api kezeles megoldasa es termekek megjelenitese az oldalon

--- Dnn_Vadnyom_HelloWorld/Controllers/ItemController.cs
+++ Dnn_Vadnyom_HelloWorld/Controllers/ItemController.cs
@@ -8,11 +8,14 @@ using DotNetNuke.Web.Mvc.Framework.ActionFilters;
 using DotNetNuke.Entities.Users;
 using DotNetNuke.Framework.JavaScriptLibraries;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace Vadnyom.Dnn.Dnn_Vadnyom_HelloWorld.Controllers
 {
     public class ItemController : DnnController
     {
+        private readonly HotcakesApiService _apiService = new HotcakesApiService();
+
         [HttpGet]
         public ActionResult Index(int step = 0, int? coatId = null, int? pantsId = null, int? bootId = null)
         {
@@ -68,10 +71,7 @@ namespace Vadnyom.Dnn.Dnn_Vadnyom_HelloWorld.Controllers
             {
                 if (postedModel.CurrentStep == 1)
                 {
-                    if (postedModel.SelectedCoatId.HasValue)
-                    {
-                        nextStep = 2;
-                    }
+                    if (postedModel.SelectedCoatId.HasValue) nextStep = 2;
                     else
                     {
                         nextStep = 1;
@@ -80,10 +80,7 @@ namespace Vadnyom.Dnn.Dnn_Vadnyom_HelloWorld.Controllers
                 }
                 else if (postedModel.CurrentStep == 2)
                 {
-                    if (postedModel.SelectedPantsId.HasValue)
-                    {
-                        nextStep = 3;
-                    }
+                    if (postedModel.SelectedPantsId.HasValue) nextStep = 3;
                     else
                     {
                         nextStep = 2;
@@ -92,10 +89,7 @@ namespace Vadnyom.Dnn.Dnn_Vadnyom_HelloWorld.Controllers
                 }
                 else if (postedModel.CurrentStep == 3)
                 {
-                    if (postedModel.SelectedBootId.HasValue)
-                    {
-                        nextStep = 4;
-                    }
+                    if (postedModel.SelectedBootId.HasValue) nextStep = 4;
                     else
                     {
                         nextStep = 3;
@@ -123,11 +117,21 @@ namespace Vadnyom.Dnn.Dnn_Vadnyom_HelloWorld.Controllers
             return View(model);
         }
 
-
         private BundleViewModel BuildModel(int step, int? coatId, int? pantsId, int? bootId)
         {
+            List<Item> items;
+            string apiError = null;
 
-            var items = GetMockItems();
+            try
+            {
+                var apiProducts = _apiService.GetProducts();
+                items = _apiService.MapToBundleItems(apiProducts);
+            }
+            catch (Exception ex)
+            {
+                items = new List<Item>();
+                apiError = "API hiba: " + ex.Message;
+            }
 
             var model = new BundleViewModel
             {
@@ -137,20 +141,22 @@ namespace Vadnyom.Dnn.Dnn_Vadnyom_HelloWorld.Controllers
                 SelectedBootId = bootId,
 
                 Coats = items.Where(x => x.Category == "Coat" && x.IsActive)
-                                         .OrderBy(x => x.SortOrder)
-                                         .ToList(),
+                             .OrderBy(x => x.SortOrder)
+                             .ToList(),
 
                 Pants = items.Where(x => x.Category == "Pants" && x.IsActive)
-                                         .OrderBy(x => x.SortOrder)
-                                         .ToList(),
+                             .OrderBy(x => x.SortOrder)
+                             .ToList(),
 
                 Boots = items.Where(x => x.Category == "Boots" && x.IsActive)
-                                         .OrderBy(x => x.SortOrder)
-                                         .ToList(),
+                             .OrderBy(x => x.SortOrder)
+                             .ToList(),
 
                 SelectedCoat = items.FirstOrDefault(x => x.ItemId == coatId),
                 SelectedPants = items.FirstOrDefault(x => x.ItemId == pantsId),
-                SelectedBoot = items.FirstOrDefault(x => x.ItemId == bootId)
+                SelectedBoot = items.FirstOrDefault(x => x.ItemId == bootId),
+
+                ErrorMessage = apiError
             };
 
             var selectedItems = items.Where(x =>
@@ -173,113 +179,5 @@ namespace Vadnyom.Dnn.Dnn_Vadnyom_HelloWorld.Controllers
 
             return model;
         }
-
-        private List<Item> GetMockItems()
-        {
-            return new List<Item>
-            {
-                new Item
-                {
-                    ItemId = 1,
-                    ItemName = "Abisko vadász mellény",
-                    ItemDescription = "Vadász mellény",
-                    Category = "Coat",
-                    Price = 68900,
-                    ImageUrl = "/Portals/0/Images/abisko-melleny.jpg",
-                    SortOrder = 1,
-                    IsActive = true
-                },
-                new Item
-                {
-                    ItemId = 2,
-                    ItemName = "Abisko 2 vadász kabát",
-                    ItemDescription = "Vadász kabát",
-                    Category = "Coat",
-                    Price = 132900,
-                    ImageUrl = "/Portals/0/Images/abisko-kabat.jpg",
-                    SortOrder = 2,
-                    IsActive = true
-                },
-                new Item
-                {
-                    ItemId = 3,
-                    ItemName = "Abisko 2.0 női vadász kabát",
-                    ItemDescription = "Női vadász kabát",
-                    Category = "Coat",
-                    Price = 109900,
-                    ImageUrl = "/Portals/0/Images/abisko-noi-kabat.jpg",
-                    SortOrder = 3,
-                    IsActive = true
-                },
-                new Item
-                {
-                    ItemId = 4,
-                    ItemName = "Pinewood vadásznadrág",
-                    ItemDescription = "Vadásznadrág",
-                    Category = "Pants",
-                    Price = 45900,
-                    ImageUrl = "/Portals/0/Images/vadasznadrag-1.jpg",
-                    SortOrder = 1,
-                    IsActive = true
-                },
-                new Item
-                {
-                    ItemId = 5,
-                    ItemName = "Abisko túranadrág",
-                    ItemDescription = "Túranadrág",
-                    Category = "Pants",
-                    Price = 52900,
-                    ImageUrl = "/Portals/0/Images/vadasznadrag-2.jpg",
-                    SortOrder = 2,
-                    IsActive = true
-                },
-                new Item
-                {
-                    ItemId = 6,
-                    ItemName = "Vízálló vadásznadrág",
-                    ItemDescription = "Vízálló nadrág",
-                    Category = "Pants",
-                    Price = 59900,
-                    ImageUrl = "/Portals/0/Images/vadasznadrag-3.jpg",
-                    SortOrder = 3,
-                    IsActive = true
-                },
-                new Item
-                {
-                    ItemId = 7,
-                    ItemName = "Vadász bakancs",
-                    ItemDescription = "Magas szárú bakancs",
-                    Category = "Boots",
-                    Price = 69900,
-                    ImageUrl = "/Portals/0/Images/bakancs-1.jpg",
-                    SortOrder = 1,
-                    IsActive = true
-                },
-                new Item
-                {
-                    ItemId = 8,
-                    ItemName = "Téli vadász bakancs",
-                    ItemDescription = "Téli bakancs",
-                    Category = "Boots",
-                    Price = 79900,
-                    ImageUrl = "/Portals/0/Images/bakancs-2.jpg",
-                    SortOrder = 2,
-                    IsActive = true
-                },
-                new Item
-                {
-                    ItemId = 9,
-                    ItemName = "Vízálló túrabakancs",
-                    ItemDescription = "Vízálló bakancs",
-                    Category = "Boots",
-                    Price = 84900,
-                    ImageUrl = "/Portals/0/Images/bakancs-3.jpg",
-                    SortOrder = 3,
-                    IsActive = true
-                }
-            };
-        }
     }
 }
-
- 
