fd25a62670e621bfcabcba68aa024e350afe15a7
Vadnyom <durugy.andrea@gmail.com>
2026-04-27 09:02:10 +0200

    test unit test project

---
+++ kliensapp/FairOrder/UnitTest.Test/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace UnitTest.Test
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
